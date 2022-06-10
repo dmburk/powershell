79f395d4a1c7c53250400760c5a338cd1eb6cc72
Michael Burk <michael@artificialgravity.co>
2022-06-09 20:43:35 -0400

    added comments

--- PSCustomObject.ps1
+++ PSCustomObject.ps1
@@ -6,6 +6,21 @@ $myObject = [PSCustomObject]@{
     Location = 'Local'
 }
 
+# save object to json
 $Path = '.\myobject.json'
 $myObject | ConvertTo-Json -depth 1 | Set-Content -Path $Path
+# load object from json
 $myObject = Get-Content -Path $Path | ConvertFrom-Json
+
+# add properties
+$myObject | Add-Member -MemberType NoteProperty -Name 'ID' -Value 'KevinMarquette'
+$myObject.ID
+# remove properties
+$myObject.psobject.properties.remove('ID')
+# dynamically accessing properties
+$myObject.Name
+# or
+$myObject.'Name'
+# or
+$property = 'Name'
+$myObject.$property
