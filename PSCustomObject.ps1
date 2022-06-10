7906e5f2d22b20bf24316373f1c1f73bcd68cf92
Michael Burk <michael@artificialgravity.co>
2022-06-09 20:35:09 -0400

    PSCustomObject example

--- PSCustomObject.ps1
+++ PSCustomObject.ps1
@@ -1,5 +1,7 @@
 # PSCustomObject Notes
-$PI = 3.14
-Write-Host "The value of `$PI is $PI"
-Write-Host 'Here is $PI'
-Write-Host "An expression $($PI + 1)" # Prints An expression 4.14
+
+$myObject = [PSCustomObject]@{
+    Name = 'Iceman'
+    Language = 'PowerShell'
+    Location = 'Local'
+}
