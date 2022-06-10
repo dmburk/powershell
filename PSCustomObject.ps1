d80bcfbd3a21128e6dd337ddc5b1220ffca2df8c
Michael Burk <michael@artificialgravity.co>
2022-06-09 20:39:30 -0400

    saved object to json

--- PSCustomObject.ps1
+++ PSCustomObject.ps1
@@ -5,3 +5,7 @@ $myObject = [PSCustomObject]@{
     Language = 'PowerShell'
     Location = 'Local'
 }
+
+$Path = '.\myobject.json'
+$myObject | ConvertTo-Json -depth 1 | Set-Content -Path $Path
+$myObject = Get-Content -Path $Path | ConvertFrom-Json
